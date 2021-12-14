2d07deee25265f85d784a82c9a01baec9bf42a6c
oscarneiva <oscarneivaeneto@gmail.com>
2021-12-14 11:04:52 -0300

    shuffle is working

--- src/controller/TourController.java
+++ src/controller/TourController.java
@@ -27,15 +27,17 @@ public class TourController {
         char label = '@'; // Character that comes before A (Dec 64)
         
         int size = pointController.getPoints().size();
-        Point[] points = new Point[size];
-        
-        for (int i = 0; i < size; i++) {
-            points[i] = pointController.getPoints().get(i);
-        }
         
         tours = new LinkedList<>();
         for (int i = 0; i < numberOfTours; i++) {
             label = (char) (label + 1);
+            
+            Point[] points = new Point[size];
+        
+            for (int j = 0; j < size; j++) {
+                points[j] = pointController.getPoints().get(j);
+            }
+            
             points = shuffle(points);
             tour = new Tour(label, points);
             tours.add(tour);
@@ -45,9 +47,9 @@ public class TourController {
     public Point[] shuffle(Point[] arr){
         Random random = new Random();
 
-        for (int i = 0; i < 20; i++) {
-            int x = random.nextInt(20);
-            int y = random.nextInt(20);
+        for (int i = 0; i < arr.length; i++) {
+            int x = random.nextInt(arr.length);
+            int y = random.nextInt(arr.length);
 
             Point aux = arr[x];
             arr[x] = arr[y];
