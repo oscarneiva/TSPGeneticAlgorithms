6bd062083d6ae8bb76c6f3b9a6958ab94310da64
oscarneiva <oscarneivaeneto@gmail.com>
2021-12-17 11:15:52 -0300

    stochastic universal ssampling finished but not tested

--- src/controller/SelectionStrategies.java
+++ src/controller/SelectionStrategies.java
@@ -5,15 +5,15 @@ import model.Tour;
 import java.util.Random;
 
 public class SelectionStrategies {
-    private TourController tourController;
     private Tour[] matingPool;
+    private TourController tourController;
     private Random random = new Random();
 
     public SelectionStrategies(){
         tourController = new TourController(10,20,20);
     }
 
-    public void rouletteWheelSelection(int numberOfElements){
+    public Tour[] rouletteWheelSelection(int numberOfElements){
         double sumFitness = 0.0;
         Tour selectedTour = null;
         matingPool = new Tour[numberOfElements];
@@ -38,13 +38,57 @@ public class SelectionStrategies {
 
             matingPool[i] = selectedTour;
         }
-        for (int i = 0; i < numberOfElements; i++) {
-            System.out.println(matingPool[i].getLabel());
-        }
+        
+        return matingPool;
     }
 
-    public void stochasticUniSampling(){
+    public Tour[] stochasticUniSampling(int numberOfGenerations){
+        double sumFitness = 0.0;
+        double[] randomValue = new double[4];
+        Tour selectedTour = null;
+        matingPool = new Tour[numberOfGenerations];
 
+        for (int i = 0; i < numberOfGenerations; i++) {
+            for (int j = 0; j < tourController.getTours().size(); j++) {
+                sumFitness = sumFitness + tourController.getTours().get(j).getFitness();
+            }
+
+            randomValue[0] = 0 + (sumFitness - 0) * random.nextDouble();
+            if (randomValue[0] < sumFitness/2) {
+                randomValue[1] = sumFitness / 2 + randomValue[0];
+                randomValue[2] = (randomValue[1] - randomValue[0])/2 + randomValue[1];
+                randomValue[3] = (randomValue[1] - randomValue[0])/2 + randomValue[0];
+                if (randomValue[2] > sumFitness) randomValue[2] = randomValue[2] - sumFitness;
+                if (randomValue[3] > sumFitness) randomValue[3] = randomValue[3] - sumFitness;
+            }else if (randomValue[0] > sumFitness/2) {
+                randomValue[1] = randomValue[0] - sumFitness / 2;
+                randomValue[2] = (randomValue[1] - randomValue[0])/2 + randomValue[1];
+                randomValue[3] = (randomValue[1] - randomValue[0])/2 + randomValue[0];
+                if (randomValue[2] > sumFitness) randomValue[2] = randomValue[2] - sumFitness;
+                if (randomValue[3] > sumFitness) randomValue[3] = randomValue[3] - sumFitness;
+            }else{
+                randomValue[1] = 3*sumFitness/4;
+                randomValue[2] = sumFitness;
+                randomValue[3] = sumFitness/4;
+            }
+
+            double rangeMin = 0;
+            double rangeMax = 0;
+
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < tourController.getTours().size(); k++) {
+                    rangeMax = rangeMax + tourController.getTours().get(k).getFitness();
+                    if(rangeMin < randomValue[j] &&
+                            randomValue[j] < rangeMax){
+                        selectedTour = tourController.getTours().get(k);
+                    }
+                    rangeMin = rangeMin + tourController.getTours().get(k).getFitness();
+                }
+                matingPool[i*4+j] = selectedTour;
+            }
+        }
+
+        return matingPool;
     }
 
     public void tournamentSelection(){
